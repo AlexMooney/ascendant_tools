0ca55598cbb04fdfe60a21ae7a1be7a5cfeccf13
Alex Mooney <AlexMMooney@gmail.com>
2021-09-05 11:01:41 -0700

    Add a param to generate number of Ascendants

--- demographics.py
+++ demographics.py
@@ -28,6 +28,7 @@ def generate_pl():
     "-p",
     "--population",
     default=1000000,
+    type=int,
     help="Population to draw from (default 1,000,000)",
 )
 @click.option(
@@ -35,29 +36,39 @@ def generate_pl():
     is_flag=True,
     help="Don't randomize the total number of Ascendants.",
 )
+@click.option(
+    "-a",
+    "--ascendants",
+    default=None,
+    type=int,
+    help="Genrate a number of Ascendants instead of a population.",
+)
 @click.option("--seed", default=None, help="Override the RNG seed.")
-def generate(population, expected_pop, seed):
+def generate(population, expected_pop, ascendants, seed):
     if seed is None:
         time = datetime.now()
         seed = time.hour * 10000 + time.minute * 100 + time.second
     random.seed(seed)
 
-    ascendants = round(ASCENDANT_FRACTION * population)
-    click.echo(
-        f"There are {ascendants:,} expected Ascendants in a population of {population:,}."
-    )
-    if expected_pop:
-        click.echo("Using the expected population.")
-    elif population < BINOMIAL_APPROXIMATION:
-        click.echo("Rolling for each person to get number of Ascendants.")
-        ascendants = sum(1 for _ in range(population) if rng() <= ASCENDANT_FRACTION)
-    else:
+    if ascendants is None:
+        ascendants = round(ASCENDANT_FRACTION * population)
         click.echo(
-            "Using the approximate binomial distribution to get number of Ascendants."
+            f"There are {ascendants:,} expected Ascendants in a population of {population:,}."
         )
-        np = population * ASCENDANT_FRACTION
-        sigma = (np * (1 - ASCENDANT_FRACTION)) ** 0.5
-        ascendants = round(random.normalvariate(np, sigma))
+        if expected_pop:
+            click.echo("Using the expected population.")
+        elif population < BINOMIAL_APPROXIMATION:
+            click.echo("Rolling for each person to get number of Ascendants.")
+            ascendants = sum(1 for _ in range(population) if rng() <= ASCENDANT_FRACTION)
+        else:
+            click.echo(
+                "Using the approximate binomial distribution to get number of Ascendants."
+            )
+            np = population * ASCENDANT_FRACTION
+            sigma = (np * (1 - ASCENDANT_FRACTION)) ** 0.5
+            ascendants = round(random.normalvariate(np, sigma))
+    else:
+        click.echo(f"Generating exactly {ascendants} Ascendants.")
 
     results = Counter(generate_pl() for _ in range(ascendants))
     keys = sorted(list(results))
